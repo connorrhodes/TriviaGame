19ec64ee6699c710ede3da8152da1de65dec882b
crhodes <connorrhodes1@yahoo.com>
2017-12-04 23:27:52 -0800

    switched topics of questions and added more

--- assets/javascript/questions-library.js
+++ assets/javascript/questions-library.js
@@ -1,32 +1,32 @@
 var questionsLibrary = [
     {
-        question: "?",
-        answers: ["", "", "", ""],
-        correctAnswer: 0,
+        question: "Whats the capitol city of Oregon?",
+        answers: ["Portland", "Salem", "Eugene", "Corvallis"],
+        correctAnswer: 1,
         image: "assets/images/"
     },
     {
-        question: " ?",
-        answers: ["", "", "", ""],
+        question: "What's Nephrology the study of?",
+        answers: ["The Kidney", "The Immune System", "The Throat", "The Eyes"],
+        correctAnswer: 0,
+        image: "assets/images"
+    },
+    {
+        question: "Whats the name of the Spy/Assassin Movie Trilogy starring Matt Damon?",
+        answers: ["The Hobbit Trilogy", "The Bourne Trilogy", "Ocean's Thirteen", "Taken 3"],
         correctAnswer: 1,
         image: "assets/images"
     },
     {
-        question: " ?",
-        answers: ["", "", "", ""],
-        correctAnswer: 1,
+        question: "What's the name of the Gorilla killed in the Cincinatti Zoo and turned into a meme?",
+        answers: ["What a lame meme", "Hurrambe", "Harambe", "Mabootuu"],
+        correctAnswer: 2,
         image: "assets/images"
     },
     {
-        question: " ?",
-        answers: ["", "", "", ""],
-        correctAnswer: 1,
-        image: "assets/images"
-    },
-    {
-        question: " ?",
-        answers: ["", "", "", ""],
-        correctAnswer: 1,
+        question: "What computer language is being used to generate this quiz?",
+        answers: ["C#", "C++", "Java", "Javascript"],
+        correctAnswer: 3,
         image: "assets/images"
     },
     {
